991ddbd44c98ba325b69a5c7154c9921cc5a50c2
ehab wasel <ehabgithub@gmail.com>
2020-11-22 00:36:08 +0200

    Ehab HW Week3 5 files + credit pproject

---
+++ Week3/EX5.js
@@ -0,0 +1,20 @@
+const cartForParty = {
+  cheps:1.7,
+  orange:2.5,
+  apple:1.5,
+  fish:3.1,
+  meat:3.25,
+};
+
+  
+  function calculateTotalPrice()
+  {
+     let totalPrice =0;
+    for(key in cartForParty){
+      totalPrice +=(cartForParty[key]);
+   
+    }
+    console.log("Total: €" , totalPrice);
+  }
+  
+calculateTotalPrice("")
