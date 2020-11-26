cdea57a69bb25a3cbcaecf93def33f8c225b4aeb
ehab wasel <ehabgithub@gmail.com>
2020-11-26 22:37:30 +0200

    I fixed all exercises according ti feedback

--- Week3/Credit.js
+++ Week3/Credit.js
@@ -7,18 +7,23 @@ function validateCreditNumber(inputtxt) {
   //this condition for length of inpute
   if(inputtxt.length<16||inputtxt.length>16) 
   { 
-  alert('Input must be 16  numbers!');
-  validate1= false;}
+  alert('Input must be 16  numbers!');}
   else{
     validate1= true;
   }
  
  //this condition for the type of inpute is only numbers of inpute
- for (let i=0;i<inputtxt.length;i++){
-    if(!((inputtxt.charCodeAt([i])>=48 && inputtxt.charCodeAt([i])<=57))){ console.log("Invalid! The input ",inputtxt," should contain only numbers!  ");validate2= false;break ; }
-    else{
-   validate2= true;
- }}
+ for (let i=0;i<inputtxt.length;i++) {
+  if (!((inputtxt.charCodeAt([i])>=48 && inputtxt.charCodeAt([i])<=57)))
+  { 
+    console.log("Invalid! The input ", inputtxt, " should contain only numbers!");
+    validate2 = false;
+    break; 
+  }
+  else {
+    validate2= true;
+  }
+}
  
 //this condition for  the inpute should contain at least 2 different types of numbers
  let bol=false ;
@@ -78,6 +83,6 @@ validate5= true;
  validateCreditNumber('a92332119c011112');
  validateCreditNumber('4444444444444444');
  validateCreditNumber('6666666666661666');
- /
+ 
  
  
